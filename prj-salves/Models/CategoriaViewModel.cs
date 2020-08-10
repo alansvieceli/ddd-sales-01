85573058a7c9a65b52677a0f8337189a8f0556b5
Alan Vieceli <alan.vieceli@softplan.com.br>
2020-08-09 22:27:21 -0300

    Cadastro de categorias. (má pratica)

---
+++ prj-salves/Models/CategoriaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace prj_sales.Models
+{
+    public class CategoriaViewModel
+    {
+        public int?  Codigo { get; set; }
+        [Required(ErrorMessage = "Faltou definir descrição da categoria")]
+        public String Descricao { get; set; }
+    }
+}
--- prj-salves/Models/ErrorViewModel.cs
+++ prj-salves/Models/ErrorViewModel.cs
@@ -5,7 +5,6 @@ namespace prj_sales.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
